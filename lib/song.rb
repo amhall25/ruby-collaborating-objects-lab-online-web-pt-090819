85e3908b0d4957cac6511ab531390ee43dd5a74a
alycia hall <amhall25@gmail.com>
2019-10-26 08:15:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
   end
   
   def artist_name= (name)
-    Artist.find_or_create_by_name(name)
+    self.artist=Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
     
   end
