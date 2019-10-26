82518722ae25a8a11c02afd7c2bbf5c2d3c37175
alycia hall <amhall25@gmail.com>
2019-10-26 07:47:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     
   def self.new_by_filename(file_name)
     songname =(file_name.split(" - ")[1])
-    @artist_name = (file_name.split(" - ")[0])
+    artist_name = (file_name.split(" - ")[0])
     self.new(songname)
     
   end
