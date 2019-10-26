8cf8dcd2b291bc6f833a4291064cbb0f7ced6124
alycia hall <amhall25@gmail.com>
2019-10-26 07:30:41 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+
+class Song
+  attr_accessor :name, :artist
+  @@all=[]
+  
+  def initialize (name, artist=nil)
+    @name= name
+    @artist= artist
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+
+    
+  def self.new_by_filename(file_name)
+    songname =(file_name.split(" - ")[0])
+    artist_name = (file_name.split(" - ")[1])
+    newsong=self.new(songname)
+    newsong.artist = Artist.find_or_create_by_name(artist_name).name
+    
+  
+    
+  end
+  
+  
+    
+
+  
+end
